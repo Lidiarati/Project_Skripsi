c7f6d3a5ec1b339de4e92ac203f5475fee8f081d
Lidiarati <ratinduet99@gmail.com>
2022-04-29 17:10:04 +0700

    tambah file lap

--- jadwal.php
+++ jadwal.php
@@ -1,11 +1,9 @@
 <?php 
 include('koneksi.php');
 session_start();
-
 if (!isset($_SESSION['username'])) {
     header("Location:login.php");
 }
-
 ?>
 <div class="row">
                 <div class="col-md-12">

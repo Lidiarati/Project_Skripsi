0d453f1769632bb15d5eb35e3ff960dce5db1b22
Lidiarati <ratinduet99@gmail.com>
2022-04-29 08:30:50 +0700

    tambah jadwal

--- tambah_jadwal.php
+++ tambah_jadwal.php
@@ -1,3 +1,11 @@
+<?php 
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location:login.php");
+}
+
+?>
 <h3>Tambah Jadwal</h3>
 <div class="panel panel-primary">
 <div class="panel-heading">
